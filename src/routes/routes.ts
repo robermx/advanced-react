f3bf7ec1cad79a38bb7074b25dd521dab3d890c8
Roberto Arenas <roberto.arenas2@icloud.com>
2023-02-01 16:37:31 -0600

    lazy-load nested routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,6 @@
 import { lazy, LazyExoticComponent } from 'react';
 // import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazy-load/pages';
+import { NoLazy } from '../01-lazy-load/pages/NoLazy';
 
 type JSXComponent = () => JSX.Element;
 
@@ -11,30 +12,21 @@ interface Route {
   linkName: string;
 }
 
-const lazy1 = lazy(() => import('../01-lazy-load/pages/LazyPage1'));
-const lazy2 = lazy(() => import('../01-lazy-load/pages/LazyPage2'));
-const lazy3 = lazy(() => import('../01-lazy-load/pages/LazyPage3'));
+const LazyLayout = lazy(() => import('../01-lazy-load/layout/LazyLayout'));
 
 export const routes: Route[] = [
   {
     key: 'abx12',
-    to: '/lazy1',
-    path: 'lazy1',
-    Component: lazy1,
+    to: '/lazy-load/',
+    path: '/lazy-load/*',
+    Component: LazyLayout,
     linkName: 'Lazy 1',
   },
   {
     key: 'abz17',
-    to: '/lazy2',
-    path: 'lazy2',
-    Component: lazy2,
-    linkName: 'Lazy 2',
-  },
-  {
-    key: 'abu32',
-    to: '/lazy3',
-    path: 'lazy3',
-    Component: lazy3,
-    linkName: 'Lazy 3',
+    to: '/no-lazy',
+    path: 'no-lazy',
+    Component: NoLazy,
+    linkName: 'No Lazy',
   },
 ];
